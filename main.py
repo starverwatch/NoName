2d00bbc5ffdea70eecdb0c87979d9c3ed0c38db5
Alexis <alexis@delogne.be>
2021-09-15 23:08:00 +0200

    No button
    J'ai enlevé les boutons

--- main.py
+++ main.py
@@ -14,22 +14,5 @@ bg = PhotoImage(file="sword.png")
 label1 = Label(root, image=bg)
 label1.place(x=0, y=0)
 
-label2 = Label(root, text="Welcome")
-label2.pack(pady=50)
-
-# Create Frame
-frame1 = Frame(root)
-frame1.pack(pady=20)
-
-# Add buttons
-button1 = Button(frame1, text="Exit")
-button1.pack(pady=20)
-
-button2 = Button(frame1, text="Start")
-button2.pack(pady=20)
-
-button3 = Button(frame1, text="Reset")
-button3.pack(pady=20)
-
 # Execute tkinter
 root.mainloop()
